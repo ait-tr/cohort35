33744ce39de901a60a6b35d475f9dbb5951ee7dc
Sergii Bugaienko <sergiibugaienko@gmail.com>
2023-10-09 21:09:24 +0200

    lesson_24 code add

---
+++ basic_programming/lesson_24/code/src/persons/BusDriver.java
@@ -0,0 +1,37 @@
+package persons;
+/*
+@date 09.10.2023
+@author Sergey Bugaienko
+*/
+
+public class BusDriver extends Person{
+
+    private String driveLicence;
+
+    private final int id;
+    private static int counter = 100;
+
+    public BusDriver(String name, int age, String driveLicence){
+        super(name, age);
+        this.driveLicence = driveLicence;
+        this.id = counter++;
+    }
+
+    public void  driverInfo() {
+        System.out.println("BusDriver "+ getName() + " id:" + id
+                + " (права номер: " + driveLicence + "); возраст: " + getAge());
+    }
+
+    public String getDriveLicence() {
+        return driveLicence;
+    }
+
+    public void setDriveLicence(String driveLicence) {
+        this.driveLicence = driveLicence;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+}
---
+++ basic_programming/lesson_24/code/src/persons/Person.java
@@ -0,0 +1,37 @@
+package persons;
+/*
+@date 09.10.2023
+@author Sergey Bugaienko
+*/
+
+public class Person {
+
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void info() {
+        System.out.println("{" + name + " возраст: " + age + "}");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+        // isKind = age < 15; так мы сделать в родителе не можем. Проблема. Надо будет решать
+    }
+}
